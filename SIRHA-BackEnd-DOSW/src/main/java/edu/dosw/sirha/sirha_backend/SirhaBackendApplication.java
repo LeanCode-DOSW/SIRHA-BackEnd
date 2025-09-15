4d4e1a4876c10532f98a7d7c8045f00bdc6708a2
Jacoboo <jacobo.diaz-a@mail.escuelaing.edu.co>
2025-09-15 18:08:28 -0500

    Jacobo: creación de la rama

---
+++ SIRHA-BackEnd-DOSW/src/main/java/edu/dosw/sirha/sirha_backend/SirhaBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.dosw.sirha.sirha_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SirhaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SirhaBackendApplication.class, args);
+	}
+
+}
---
+++ SIRHA-BackEnd-DOSW/src/test/java/edu/dosw/sirha/sirha_backend/SirhaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.sirha.sirha_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SirhaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
