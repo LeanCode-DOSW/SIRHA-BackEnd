a29a352f48cea2621ce1a6ef675ae4e69517d1f4
Jacoboo <jacobo.diaz-a@mail.escuelaing.edu.co>
2025-09-15 19:16:35 -0500

    Jacobo: clases

---
+++ SIRHA-BackEnd-DOSW/src/main/java/edu/dosw/sirha/sirha_backend/GroupController.java
@@ -0,0 +1,11 @@
+package edu.dosw.sirha.sirha_backend;
+
+import java.util.List;
+
+public class GroupController {
+    private GroupService groupService;
+
+    public ResponseEntity<List<GroupDTO>> getGroupsBySubject(String subject){
+
+    }
+}
