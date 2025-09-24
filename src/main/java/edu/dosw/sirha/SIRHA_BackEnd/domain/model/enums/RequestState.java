99be303940d4508b208327e559c7bc6be3a26fdc
Andres Felipe Cardozo Martinez <andres.cardozo-m@mail.escuelaing.edu.co>
2025-09-24 08:47:38 -0500

    Revert "Feature/registro autenticacion estudiantes"

--- src/main/java/edu/dosw/sirha/SIRHA_BackEnd/domain/model/enums/RequestState.java
+++
@@ -1,8 +0,0 @@
-package edu.dosw.sirha.SIRHA_BackEnd.domain.model.enums;
-
-public enum RequestState {
-    PENDIENTE,
-    APROBADA,
-    RECHAZADA,
-    EN_REVISION
-}
--- src/main/java/edu/dosw/sirha/SIRHA_BackEnd/domain/port/GroupState.java
+++
@@ -1,7 +0,0 @@
-package edu.dosw.sirha.SIRHA_BackEnd.domain.port;
-
-import edu.dosw.sirha.SIRHA_BackEnd.domain.model.*;
-
-public interface GroupState {
-    void inscribirEstudiante(Group grupo, Student estudiante);
-}
