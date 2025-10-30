7864b14b84a32637181ad4c6187c10b859190128
Andrew7480 <andrewc7480@gmail.com>
2025-10-30 03:34:51 -0500

    fixing project structure, removing old files

--- src/main/java/edu/dosw/sirha/sirha_backend/domain/model/enums/Role.java
+++
@@ -1,5 +0,0 @@
-package edu.dosw.sirha.sirha_backend.domain.model.enums;
-
-public enum Role {
-    STUDENT, DEAN, ADMIN
-}
--- src/main/java/edu/dosw/sirha/sirha_backend/domain/model/enums/SemaforoColores.java
+++
@@ -1,9 +0,0 @@
-package edu.dosw.sirha.sirha_backend.domain.model.enums;
-
-public enum SemaforoColores {
-    VERDE,    // aprobada
-    AMARILLO, // cursando
-    ROJO,     // reprobada
-    GRIS      // no cursada
-}
-    
